fe9f2d282076851103c7ec7da1f40a7f7d2bc149
Bruno Fosados <bruno.fosados@gmail.com>
2018-04-04 12:26:41 -0500

    [ADD] Planning stage

--- analytics/models.py
+++ analytics/models.py
@@ -4,3 +4,27 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Record(models.Model):
+    """
+    Main data structure
+    """
+    ocid = models.CharField('OCID', max_length=50)
+    planning = models.ForeignKey('Planning')
+
+    def __str__(self):
+        return '{}'.format(self.ocid)
+
+
+class Planning(models.Model):
+    """
+    First stage of the open contratcs standar please check documentation at: url-here
+    """
+
+    budget_break_down_url = models.URLField()
+    budget_break_down_description = models.CharField('Budget Description', max_length=50)
+    budget_break_down_id = models.IntegerField()
+    budget_classification_level_label = models.CharField('Level Label', max_length=50)
+    budget_classification_level_description = models.CharField('Level description',  max_length=50)
+
+    def __str__(self):
+        return '{}'.format(self.budget_classification_level_label)
