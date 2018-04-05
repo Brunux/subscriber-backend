59df7047181a857393abaa808a006979cdefe3ed
Rene Torres <ingrenetorres@gmail.com>
2018-04-05 01:03:05 -0500

    adding models

--- analytics/admin.py
+++ analytics/admin.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from models import Planning, Record, Tender, Publisher, Buyer, Tag
+from models import Planning, Record, Tender, Publisher, Buyer, Tag, Contracts, Parties, Awards
 
 # Register your models here.
 
@@ -27,7 +27,21 @@ class EventAdmin(admin.ModelAdmin):
 class EventAdmin(admin.ModelAdmin):
     list_display = ('buyerName', 'buyerId',)
 
+@admin.register(Contracts)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('contract_title', 'status', 'contract_details', 'amount', 'period',)
+
+@admin.register(Parties)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('partie_id', 'partie_name', 'contact_name', 'contact_name', 'contact_email',)
+
+@admin.register(Awards)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('award_title', 'award_description', 'award_id',)
 
 @admin.register(Tag)
 class EventAdmin(admin.ModelAdmin):
     list_display = ('cero',)
+
+
+
--- analytics/models.py
+++ analytics/models.py
@@ -76,6 +76,10 @@ class Record(models.Model):
     publisher = models.ForeignKey('Publisher')
     buyer = models.ForeignKey('Buyer')
     tag = models.ForeignKey('Tag')
+    contratcs = models.ForeignKey('Contracts')
+    parties = models.ForeignKey('Parties')
+    awards = models.ForeignKey('Awards')
+
 
     def __str__(self):
         return '{}'.format(self.ocid)
@@ -83,7 +87,7 @@ class Record(models.Model):
 
 class Planning(models.Model):
     """
-    First stage of the open contratcs standar please check documentation at: url-here
+    First stage of the open Planning standar please check documentation at: url-here
     """
 
     budget_break_down_url = models.URLField()
@@ -95,3 +99,62 @@ class Planning(models.Model):
     def __str__(self):
         return '{}'.format(self.budget_classification_level_label)
 
+
+class Contracts(models.Model):
+    """
+    First stage of the open contratcs standar please check documentation at: url-here
+    """
+
+    value_with_tax = models.CharField(max_length=50)
+    contract_title = models.CharField(max_length=250)
+    implementation = models.CharField(max_length=250)
+    suppliers = models.CharField(max_length=50)
+    buyers = models.CharField(max_length=50)
+    date_signed = models.CharField(max_length=50)
+    period = models.CharField(max_length=50)
+    award_id = models.CharField(max_length=50)
+    contract_id = models.IntegerField()
+    amount = models.IntegerField()
+    status = models.CharField(max_length=50)
+    contract_details = models.CharField(max_length=250)
+
+    def __str__(self):
+        return '{}'.format(self.contract_id, self.contract_title, self.status, self.contract_details)
+
+
+class Parties(models.Model):
+    """
+    First stage of the open parties standar please check documentation at: url-here
+    """
+
+    contact_telephone = models.CharField(max_length=50)
+    contact_email   = models.CharField(max_length=50)
+    contact_name = models.CharField(max_length=50)
+    street_adress =  models.CharField(max_length=250)
+    region = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=50)
+    country_name = models.CharField(max_length=50)
+    partie_name = models.CharField(max_length=250)
+    partie_id = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return '{}'.format(self.partie_id, self.partie_name, self.contact_name, self.contact_email)
+
+
+class Awards(models.Model):
+    """
+    First stage of the open Awards standar please check documentation at: url-here
+    """
+
+    award_title = models.CharField('Awards title', max_length=250)
+    award_description = models.CharField('Awards description', max_length=250)
+    award_id = models.CharField('Awards id', max_length=50)
+    currency =  models.CharField('currency', max_length=15)
+    amount = models.IntegerField()
+    supplier_name = models.CharField('Supplier name',max_length=250)
+    supplier_id = models.CharField('Supplier id',max_length=30)
+    contract_start_date = models.DateTimeField()
+    contract_end_date = models.DateTimeField()
+
+    def __str__(self):
+        return '{}'.format(self.award_title, self.award_description, self.award_id, )
